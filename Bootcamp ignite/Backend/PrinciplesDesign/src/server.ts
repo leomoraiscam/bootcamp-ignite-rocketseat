2db20a5297d9d6d16a314f5133fbb1a3995a2998
lmorais <leozonnn@gmail.com>
2021-03-17 15:17:03 -0300

    refactor in routes

--- Bootcamp ignite/Backend/PrinciplesDesign/src/server.ts
+++ Bootcamp ignite/Backend/PrinciplesDesign/src/server.ts
@@ -1,13 +1,11 @@
 import express from 'express';
 
-import categoriesRoutes from './routes/categories.routes';
-import speficationRoutes from './routes/specification.routes';
+import routes from './routes';
 
 const app = express();
 
 app.use(express.json());
-app.use('/categories', categoriesRoutes);
-app.use('/specifications', speficationRoutes);
+app.use(routes);
 
 app.listen(3333, () => {
   console.log('🚀 Server is running');
