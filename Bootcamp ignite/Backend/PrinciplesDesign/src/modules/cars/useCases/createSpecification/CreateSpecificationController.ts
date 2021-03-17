44da550ec7d98f05aa9c989c9c3143f6f7d80d2c
lmorais <leozonnn@gmail.com>
2021-03-17 15:44:24 -0300

    upload of images with use cases

--- Bootcamp ignite/Backend/PrinciplesDesign/src/modules/cars/useCases/createSpecification/CreateSpecificationController.ts
+++ Bootcamp ignite/Backend/PrinciplesDesign/src/modules/cars/useCases/createSpecification/CreateSpecificationController.ts
@@ -1,9 +1,11 @@
+import { Request, Response } from 'express';
+
 import CreateSpecificationUseCase from './CreateSpecificationUseCase';
 
 class CreateSpecificationController {
   constructor(private createSpecificationUseCase: CreateSpecificationUseCase) {}
 
-  handle(request, response): Response {
+  handle(request: Request, response: Response): Response {
     const { name, description } = request.body;
 
     this.createSpecificationUseCase.execute({
---
+++ Bootcamp ignite/Backend/PrinciplesDesign/src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
@@ -0,0 +1,7 @@
+class ImportCategoryUseCase {
+  execute(file: any) {
+    console.log('use case file', file);
+  }
+}
+
+export default ImportCategoryUseCase;
