fca56e1d12e1052688539b99a87ae0c916450464
lmorais <leozonnn@gmail.com>
2021-03-17 13:38:39 -0300

    implementation useCase pattern for create a category

--- Bootcamp ignite/Backend/PrinciplesDesign/src/routes/categories.routes.ts
+++ Bootcamp ignite/Backend/PrinciplesDesign/src/routes/categories.routes.ts
@@ -1,7 +1,7 @@
 import { Router } from 'express';
 
 import CategoriesRepository from '../modules/cars/repositories/CategoriesRepository';
-import CreateCategoriesService from '../modules/cars/services/CreateCategoryService';
+import createCategoryController from '../modules/cars/useCases/createCategory';
 
 const categoriesRoutes = Router();
 const categoriesRepository = new CategoriesRepository();
@@ -13,18 +13,7 @@ categoriesRoutes.get('/', (request, response) => {
 });
 
 categoriesRoutes.post('/', (request, response) => {
-  const { name, description } = request.body;
-
-  const createCategoriesService = new CreateCategoriesService(
-    categoriesRepository
-  );
-
-  createCategoriesService.execute({
-    name,
-    description,
-  });
-
-  return response.status(201).send();
+  return createCategoryController.handle(request, response);
 });
 
 export default categoriesRoutes;
